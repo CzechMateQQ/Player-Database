679bf478c6aa72e81bbeb026c3110f91de50f8dc
Jesse <jessesimurdak@gmail.com>
2019-12-08 22:54:35 -0800

    start

---
+++ PlayerDatabase/main.cpp
@@ -0,0 +1,15 @@
+#include "Player.h"
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+	cout << "Please enter number of players" << endl;
+	Player players[10];
+
+	while(true){}
+
+	return 0;
+}
